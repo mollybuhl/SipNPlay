5640132548612db863b0469eb8dc47cd501ab857
tranenn <aamandatran@gmail.com>
2023-11-21 10:26:19 +0100

    Added variables to css
    To store color values

--- javascript/truthORDare.js
+++ javascript/truthORDare.js
@@ -4,11 +4,15 @@ chooseTruthORDare()
 
 async function chooseTruthORDare() {
     main.innerHTML = `
-        <h1>Truth OR Dare?</h1>
-        <h2>It's <span>Lasse</span>'s turn</h2>
+        <div id="truthORDareWrapper">
+            <h1>Truth OR Dare?</h1>
+            <h2>It's <span>Lasse</span>'s turn</h2>
 
-        <button>Truth</button>
-        <div>OR</div>
-        <button>Truth</button>
-        `;
+            <section>
+                <button id="truth">Truth</button>
+                <div>OR</div>
+                <button id="dare">Dare</button>
+            </section>
+        </div>
+    `;
 }
