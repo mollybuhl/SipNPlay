9feabd50cfa84d8bdf319f8807242a1a92d8c7ac
tranenn <aamandatran@gmail.com>
2023-11-21 09:41:12 +0100

    Initial HTML structure for truth or dare

---
+++ javascript/truthORDare.js
@@ -0,0 +1,14 @@
+"use strict";
+
+chooseTruthORDare()
+
+async function chooseTruthORDare() {
+    main.innerHTML = `
+        <h1>Truth OR Dare?</h1>
+        <h2>It's <span>Lasse</span>'s turn</h2>
+
+        <button>Truth</button>
+        <div>OR</div>
+        <button>Truth</button>
+        `;
+}
